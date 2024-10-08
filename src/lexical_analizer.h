6ffeab127a23db98aa7f622385fc6c57a413fb82
Eduardo Perottoni <edu.perottoni@gmail.com>
2024-10-08 02:19:21 -0300

    Defining faster methods to LexicalAnalyzer

--- src/lexical_analizer.h
+++ src/lexical_analizer.h
@@ -1,14 +1,48 @@
 #ifndef LEXICAL_H
 #define LEXICAL_H
 
+#include <unordered_map>
+#include <stdexcept>
+#include "token.h"
 using namespace std;
 
-class LexicalAnalyzer {
+class InvalidCharacterException : public std::exception {
 public:
+    explicit InvalidCharacterException(char ch)
+        : message("Character not found: " + string(1, ch)) {}
+
+    const char* what() const noexcept override {
+        return message.c_str();
+    }
+
+private:
+    string message;
+};
+
+class LexicalAnalyzer {
+private:
+    enum charType {
+        LETTER,
+        DIGIT,
+        WS,
+    };
+
+    unordered_map<char, charType> charTypeMap;
+
+    bool isLetter(const char c);
+    bool isDigit(const char c);
+    bool isWS(const char c);
+
+    charType findOnMap(const char c);
+
+public:
+    LexicalAnalyzer();
 
     void sayHello();
 
     void getIdentifier();
+    void getNumber();
+    void getOther();
 };
 
 #endif
--- src/lexical_analyzer.cpp
+++ src/lexical_analyzer.cpp
@@ -3,6 +3,46 @@
 
 using namespace std;
 
+
+
 void LexicalAnalyzer::sayHello() {
     cout << "Hello from LexicalAnalyser" << endl;
+    cout << findOnMap('\t') << endl;
+}
+
+LexicalAnalyzer::LexicalAnalyzer() {
+    // Fill map with letters (A-Z, a-z)
+    for (char c = 'A'; c <= 'Z'; ++c) {
+        charTypeMap[c] = LETTER;
+        charTypeMap[c + 32] = LETTER;  // add lowercase letters
+    }
+    // Fill map with digits
+    for (char c = '0'; c <= 9; ++c) {
+        charTypeMap[c] = DIGIT;
+    }
+
+    charTypeMap[' '] = WS;
+    charTypeMap['\t'] = WS;
+    charTypeMap['\n'] = WS;
+}
+
+bool LexicalAnalyzer::isLetter(const char c) {
+    return true;
+}
+
+bool LexicalAnalyzer::isDigit(const char c) {
+    return true;
+}
+
+bool LexicalAnalyzer::isWS(const char c) {
+    return true;
+}
+
+LexicalAnalyzer::charType LexicalAnalyzer::findOnMap(const char c) {
+    auto it = charTypeMap.find(c);
+    cout << "Tamanho do mapa: " << charTypeMap.size() << endl;
+    if (it != charTypeMap.end())
+        return it->second;
+    else
+        throw InvalidCharacterException(c);
 }
